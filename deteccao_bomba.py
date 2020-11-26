33595db9578fa8dcb32a1224fc0c115748b309d4
Leonardo Lima <leo.lima21051990@gmail.com>
2020-11-26 11:19:48 -0300

    bomb det x

---
+++ deteccao_bomba.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python 
+import cv2
+import numpy as np
+
+imagem1 = cv2.imread('bomba8.jpeg')
+imagem2 = cv2.imread('bomba8.jpeg')
+imagem3 = cv2.imread('bomba8.jpeg')
+
+classificador1 = cv2.CascadeClassifier('cascade1.xml')
+classificador2 = cv2.CascadeClassifier('cascade2.xml')
+classificador3 = cv2.CascadeClassifier('cascade3.xml')
+
+imagemcinza = cv2.cvtColor(imagem1, cv2.COLOR_BGR2GRAY)
+
+# deteccoes1 = classificador1.detectMultiScale(imagemcinza)
+# deteccoes2 = classificador2.detectMultiScale(imagemcinza)
+# deteccoes3 = classificador3.detectMultiScale(imagemcinza)
+
+deteccoes1 = classificador1.detectMultiScale(imagemcinza, scaleFactor=1.1,
+                                           minNeighbors=30,
+                                           minSize=(10,10),
+                                           maxSize=(3000,3000))
+
+deteccoes2 = classificador2.detectMultiScale(imagemcinza, scaleFactor=1.1,
+                                           minNeighbors=30,
+                                           minSize=(10,10),
+                                           maxSize=(3000,3000))
+
+deteccoes3 = classificador3.detectMultiScale(imagemcinza, scaleFactor=1.1,
+                                           minNeighbors=30,
+                                           minSize=(10,10),
+                                           maxSize=(3000,3000))
+
+
+for (x, y, l, a) in deteccoes1:
+    cv2.rectangle(imagem1, (x, y), (x + l, y + a), (0,255,0), 2)
+
+for (x, y, l, a) in deteccoes2:
+    cv2.rectangle(imagem2, (x, y), (x + l, y + a), (0,255,0), 2)
+
+for (x, y, l, a) in deteccoes3:
+    cv2.rectangle(imagem3, (x, y), (x + l, y + a), (0,255,0), 2)
+
+cv2.imshow('class 1', imagem1)
+cv2.imshow('class 2', imagem2)
+cv2.imshow('class 3', imagem3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1,
+#                                            minNeighbors=20,
+#                                            minSize=(100,100),
+#                                            maxSize=(3000,3000))
+
+# deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1,
+#                                            minNeighbors=21,
+#                                            minSize=(5,5),
+#                                            maxSize=(3000,3000))                                           
+
+# print(deteccoes)
+# print(len(deteccoes))
+
+# for (x, y, l, a) in deteccoes:
+#     cv2.rectangle(imagem, (x, y), (x + l, y + a), (0,255,0), 2)
+
+# cv2.imshow('Detector de faces', imagem)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
--- test_bomb_detector.py
+++ test_bomb_detector.py
@@ -3,22 +3,14 @@ from classes.bomb_detector import Bomb_detector
 import cv2
 import numpy as np
 
-# create color detectors
-bomb_detect = Bomb_detector(np.array([136, 87, 111]), 
-                            np.array([180, 255, 255]),
-                            np.array([25, 52, 72]), 
-                            np.array([102, 255, 255]),                      
-                            np.array([94, 80, 2]), 
-                            np.array([120, 255, 255]),                      
-                            np.array([20, 100, 100]), 
-                            np.array([32, 255, 255]))
+image = cv2.imread('images/bomba8.jpeg')
+classificador1 = cv2.CascadeClassifier('cascade2.xml')
 
-# read a image
-image = cv2.imread('images/bomba2.jpeg')
+# create bomb detectors
+bomb_detect = Bomb_detector(classificador1, 70)
+bmb, ident = bomb_detect.bomb_det(image)
 
-bmb = bomb_detect.color_det(image)
-
-cv2.imshow("bomb", bmb)
+cv2.imshow("bomb_det", bmb)
 
 cv2.waitKey(0)    
 cv2.destroyAllWindows()
